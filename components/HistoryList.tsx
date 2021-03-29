d03a9cab4cf080e787baef05eafa1c1f6eeb3820
Victor A <viktoray007@gmail.com>
2021-03-29 15:58:20 +0300

    add no translation check

--- components/HistoryList.tsx
+++ components/HistoryList.tsx
@@ -21,6 +21,22 @@ const StyledText = styled.Text`
   font-size: 14px;
 `;
 
+const StyledEmpty = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledTitle = styled.Text`
+  font-size: 18px;
+  font-weight: 500;
+  color: #bfbfbf;
+`;
+
+const StyledDescription = styled.Text`
+  font-size: 14px;
+  color: #bfbfbf;
+`;
+
 const StyledContainer = styled.View`
   flex: 1;
   display: flex;
@@ -28,6 +44,7 @@ const StyledContainer = styled.View`
   width: 95%;
   margin-top: 25px;
   align-items: center;
+  justify-content: center;
   background-color: #fff;
   border-top-right-radius: 10px;
   border-top-left-radius: 10px;
@@ -41,7 +58,7 @@ export const StyledHistoryItem = styled.TouchableOpacity`
   border: 1px solid #eee;
   border-left-width: 0;
   border-right-width: 0;
-  border-bottom-width: 0;
+  border-top-width: 0;
 `;
 
 const styles = StyleSheet.create({
@@ -62,6 +79,7 @@ const HistoryList: FC<{
   const [translations, setTranslations] = useState<Translation[]>([]);
 
   const currentUser = useAuthentication();
+
   useEffect(() => {
     const app = firebase.app(FIREBASE_APP);
     let unsubscribe: () => void;
@@ -103,28 +121,40 @@ const HistoryList: FC<{
         favorite: !translation.favorite,
       });
   };
+
   return (
     <StyledView>
       <StyledContainer>
-        <FlatList
-          style={styles.flatList}
-          keyExtractor={(_, index) => String(index)}
-          data={translations}
-          renderItem={({ item, index }) => (
-            <StyledHistoryItem key={index} onPress={() => onSelect(item)}>
-              <View>
-                <StyledText numberOfLines={1}>{item.from}</StyledText>
-                <StyledText numberOfLines={1}>{item.to}</StyledText>
-              </View>
-              <AntDesign
-                name={item.favorite ? "star" : "staro"}
-                size={24}
-                color={item.favorite ? "gold" : "black"}
-                onPress={() => onToggleFavorite(item)}
-              />
-            </StyledHistoryItem>
-          )}
-        />
+        {favorites && !translations.length ? (
+          <StyledEmpty>
+            <AntDesign name={"star"} size={64} color={"#bfbfbf"} />
+
+            <StyledTitle>No Favorites</StyledTitle>
+            <StyledDescription>
+              Your favorite translations will appear here.
+            </StyledDescription>
+          </StyledEmpty>
+        ) : (
+          <FlatList
+            style={styles.flatList}
+            keyExtractor={(_, index) => String(index)}
+            data={translations}
+            renderItem={({ item, index }) => (
+              <StyledHistoryItem key={index} onPress={() => onSelect(item)}>
+                <View>
+                  <StyledText numberOfLines={1}>{item.from}</StyledText>
+                  <StyledText numberOfLines={1}>{item.to}</StyledText>
+                </View>
+                <AntDesign
+                  name={item.favorite ? "star" : "staro"}
+                  size={24}
+                  color={item.favorite ? "gold" : "black"}
+                  onPress={() => onToggleFavorite(item)}
+                />
+              </StyledHistoryItem>
+            )}
+          />
+        )}
       </StyledContainer>
     </StyledView>
   );
